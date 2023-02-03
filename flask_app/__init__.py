1fdee8fe94b7f416fb926f7127c3fc4a87442c5b
Kate Anderson <kaanders00@gmail.com>
2023-02-03 14:01:09 -0500

    first commit

---
+++ flask_app/__init__.py
@@ -0,0 +1,19 @@
+# __init__.py
+from flask import Flask
+app = Flask(__name__)
+app.secret_key = "shhhhhh"
+
+DATABASE = "globiday_schema"
+
+# Kander87.mysql.pythonanywhere-services.com
+# Kander87.mysql.pythonanywhere-services.com
+
+# Use these settings in your web applications.
+
+# Database host address:Kander87.mysql.pythonanywhere-services.com
+# Username:Kander87
+# Your databases:
+# Click a database's name to start a MySQL console logged in to it.
+
+# Name
+# Kander87$default
